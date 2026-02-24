1b1ef0d858b61e7737250784d156a18403f4b917
unknown <pp0238@srmist.edu.in>
2026-02-24 16:08:45 +0530

    UC13

---
+++ App/src/UC13.java
@@ -0,0 +1,40 @@
+/**
+ * ===============================================================
+ * MAIN CLASS – UseCase13PalindromeCheckerApp
+ * Use Case 13: Performance Comparison
+ *
+ * Author  : Veeranjaneya
+ * Version : 1.0
+ * ===============================================================
+ */
+
+public class UC13 {
+
+    public static boolean checkPalindrome(String s) {
+        int start = 0;
+        int end = s.length() - 1;
+
+        while (start < end) {
+            if (s.charAt(start) != s.charAt(end))
+                return false;
+            start++;
+            end--;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+
+        String input = "level";
+
+        long startTime = System.nanoTime();
+
+        boolean result = checkPalindrome(input);
+
+        long endTime = System.nanoTime();
+
+        System.out.println("Input : " + input);
+        System.out.println("Is Palindrome? : " + result);
+        System.out.println("Execution Time : " + (endTime - startTime) + " ns");
+    }
+}
