00dd71d88a6cf5969ab536098f434c08f4f1f5a0
unknown <pp0238@srmist.edu.in>
2026-02-24 14:31:35 +0530

    Veeranjaneya45  Added UC2

--- App/src/Main.java
+++ App/src/Main.java
@@ -1,57 +1,37 @@
 /**
  * ===============================================================
- * MAIN CLASS – UseCase6PalindromeCheckerApp
+ * MAIN CLASS – UseCase7PalindromeCheckerApp
  * ===============================================================
- * Use Case 6: Queue + Stack Palindrome Checker
+ * Use Case 7: Deque Based Optimized Palindrome Checker
  *
- * Description:
- * This program checks whether a given string is a palindrome
- * using two data structures:
- * 1. Queue  (FIFO – First In First Out)
- * 2. Stack  (LIFO – Last In First Out)
- *
- * Characters are inserted into both structures and then
- * compared by removing from queue front and stack top.
- * If all characters match → String is Palindrome.
- *
- * Author  : Veeranjaneya
+ * Author  : Veeranjaneya45
  * Version : 1.0
  * ===============================================================
  */
 
 import java.util.*;
 
-public class Main {
+public class Main{
 
     public static void main(String[] args) {
 
-        // Define input string to validate
-        String input = "civic";
+        String input = "refer";
 
-        // Create Queue (FIFO)
-        Queue<Character> queue = new LinkedList<>();
+        Deque<Character> deque = new ArrayDeque<>();
 
-        // Create Stack (LIFO)
-        Stack<Character> stack = new Stack<>();
-
-        // Insert characters into Queue and Stack
-        for(char c : input.toCharArray()) {
-            queue.add(c);
-            stack.push(c);
+        for (char c : input.toCharArray()) {
+            deque.addLast(c);
         }
 
-        // Flag to track palindrome status
         boolean isPalindrome = true;
 
-        // Compare Queue dequeue with Stack pop
-        while(!queue.isEmpty()) {
-            if(queue.remove() != stack.pop()) {
+        while (deque.size() > 1) {
+            if (deque.removeFirst() != deque.removeLast()) {
                 isPalindrome = false;
                 break;
             }
         }
 
-        // Print result
         System.out.println("Input : " + input);
         System.out.println("Is Palindrome? : " + isPalindrome);
     }
